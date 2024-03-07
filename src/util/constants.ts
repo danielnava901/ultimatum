64cb6a4247b15065bf4b9449057bd46b02a009a0
daniel <danielnava901@hotmail.com>
2024-03-06 19:25:42 -0600

    ultimatum wrapper

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -5,17 +5,17 @@ export const startEFDate = '2024-6-16'
 export const endDateEF = '2024-11-15'
 export const daysOfYear = 366; /*2024*/
 
-export const dayOfYear = (date: any) =>
+export const dayOfYear : any = (date: any) =>
     Math.ceil((date - new Date(date.getFullYear(), 0, 0)) / 86_400_000);
 
 export const dayNumToDate = (num: number) => new Date(new Date().getFullYear(), 0, num);
 
 export const dayNumToDateLocal = (num: number) => {
-    let d = new Date(new Date().getFullYear(), 0, num);
+    let d : any = new Date(new Date().getFullYear(), 0, num);
     return `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
 };
 
 export const todayToLocalStr = () => {
-    let d = new Date();
+    let d : any = new Date();
     return `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
 }
