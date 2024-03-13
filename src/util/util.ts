dad6d48f618bad5ce321ea4946e615740b6b23d5
daniel <danielnava901@hotmail.com>
2024-03-13 10:38:09 -0600

    eslint

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,9 +1,9 @@
-export function debounce<T extends unknown[], U>(func : any, time: number) {
+export function debounce(func : any, time: number) {
     let wait : number = time;
     let timeout : any;
 
 
-    return function inner(this: typeof inner,...args: T) {
+    return function inner(this: typeof inner,...args) {
         let _this : any = this;
 
         for (let _len : any = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
