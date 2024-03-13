d96aca3525d2746406e63a4a692b0e210cd6eb61
daniel <danielnava901@hotmail.com>
2024-03-13 10:53:40 -0600

    func this []

--- src/util/util.ts
+++ src/util/util.ts
@@ -3,7 +3,7 @@ export function debounce(func : any, time: number) {
     let timeout : any;
 
 
-    return function inner(...args) {
+    return function inner(...args : any[]) {
         for (let _len : any = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
             args[_key] = arguments[_key];
         }
